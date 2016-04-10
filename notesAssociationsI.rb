f194675c07a6ef66bb52035814820b242de1db0d
santana kenner <sskenner@gmail.com>
2016-04-10 14:57:02 +0000

    create app directory

---
+++ notesAssociationsI.rb
@@ -0,0 +1,13 @@
+# Associations I
+# Use assocations to model one-to-many relationships.
+# 1.Adding More Data
+# https://help.github.com/articles/generating-an-ssh-key/
+
+# 2.Models I
+# 3.Models II
+# 4.Display all tags
+# 5.Show a tag
+# 6.Show a destination
+# 7.Update a destination I
+# 8.Update a destination II
+# 9.Generalizations
